03ceb12a222953c45b99902d04daeb02cce2c828
kevin <kevin.richey@gmail.com>
2014-03-21 01:05:50 -0400

    Revised & simplified

--- blockout.py
+++ blockout.py
@@ -1,63 +1,94 @@
- ! /usr/bin/env python
+#! /usr/bin/env python
 
 import pygame
 
-# Setup the game state
+##### Constants ################################
+
+block_color = "blue"
+paddle_color = "white"
+ball_color = "yellow"
+
+ball_size = 20
+ball_speed = 1
+paddle_size = 20
+paddle_speed = 2
+
+##### Setup ####################################
+
 pygame.init()
 clock = pygame.time.Clock()
 running = True
 game_on = True
 
-# Setup the game board
+# This function draws a text message on the screen.
+def draw_text(message, top_left):
+	font = pygame.font.Font(None, 36)
+	text = font.render(message, 1, (255,255,255))
+	textpos = text.get_rect()
+	textpos.topleft = top_left
+	screen.blit(text, textpos)
+
+#### Screen ######################################
+
+# Setup the screen and game_board
+pygame.display.set_caption("Blockout!")
 screen = pygame.display.set_mode((640,400))
 game_board = screen.get_rect()
 
-# Start the ball off-center, moving at an angle
-ball = pygame.Rect(0,0, 20,20)
+#### Game Pieces ######################################
+
+# Start the ball in the center, moving diagonally down.
+ball = pygame.Rect(0, 0, ball_size, ball_size)
 ball.center = game_board.center
 ball.centerx -= 100
 ball_dx = 1
-ball_dy = 1
+ball_dy = ball_speed
 
 # Put a rectangular paddle near the bottom
-paddle = pygame.Rect(0, game_board.bottom-50, 60,10)
+paddle = pygame.Rect(0, game_board.bottom-50, paddle_size, 10)
 paddle.centerx = game_board.centerx
 paddle_dx = 0
-paddle_speed = 3
 
-# Pile o' Bricks
-bricks = [ 
+# Pile o' Blocks
+blocks = [ 
 	pygame.Rect((row*64)+2, (column*25)+40, 60,20) 
 	for row in range(10) 
 	for column in range(5) ]
 
-### The Main Loop
+##### Game Play Loop ##################################
 
 while running:
-	clock.tick(100)
 
-	# Get user input
+	# Run 60 frames per second
+	clock.tick(60)
+
+	##### User Input ###################################
+
+	# End the game when player closes the window.
 	event = pygame.event.poll()
 	if event.type == pygame.QUIT:
 		running = False
 
+	# Move paddle left and right with arrow keys.
 	elif event.type == pygame.KEYDOWN:
 		if event.key == pygame.K_LEFT:
 			paddle_dx = -paddle_speed
 		elif event.key == pygame.K_RIGHT:
 			paddle_dx = paddle_speed
-
 	elif event.type == pygame.KEYUP:
 		if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
 			paddle_dx = 0
 
+	##### Game Rules ###################################
+
+	# If that was the last block, you win!
+	if not blocks:
+		game_on = False
+		end_message = "You Win!"
+	
 	# Bounce ball off the paddle.
-	# Change angle by where the ball hits the paddle.
 	if paddle.colliderect(ball):
 		ball_dy = -ball_dy
-		ball_dx = (ball.centerx - paddle.centerx) / 15
-
-	## Bounce ball off the edges
 
 	# Bounce off the right edge
 	if ball_dx > 0 and ball.right > game_board.right:
@@ -69,60 +100,58 @@ while running:
 
 	# Bounce off the top edge
 	if ball_dy < 0 and ball.top < game_board.top:
-		ball_dy = 1
+		ball_dy = -ball_dy
 
 	# Game Over when ball hits the bottom
 	elif ball_dy > 0 and ball.bottom > game_board.bottom:
 		game_on = False
 		end_message = "Game Over"
 
-	# Ball hits a brick
-	hit = ball.collidelist(bricks)
+	# Ball hits a block
+	hit = ball.collidelist(blocks)
 	if hit > -1:
-		# Erase the brick
-		brick = bricks.pop(hit)
+		# Erase the block
+		block = blocks.pop(hit)
 		# Bounce off the left edge
-		if ball_dx > 0 and brick.collidepoint(ball.midright):
+		if ball_dx > 0 and block.collidepoint(ball.midright):
 			ball_dx = -ball_dx
 		# Bounce off the right edge
-		elif ball_dx < 0 and brick.collidepoint(ball.midleft):
+		elif ball_dx < 0 and block.collidepoint(ball.midleft):
 			ball_dx = -ball_dx
-		# Bounce off the top or bottom edges
+		# Bounce off the top and bottom edges
 		else:
 			ball_dy = -ball_dy
-		# If that was the last brick, you win!
-		if not bricks:
-			game_on = False
-			end_message = "You Win!"
-	
-	### Draw game on the screen
+
+	# Move the paddle
+	paddle.left += paddle_dx
+
+	# Move the ball
+	ball.centerx += ball_dx
+	ball.centery += ball_dy
+
+	##### Draw the Screen ##############################
 
 	# Start with a blank screen, erase everything
+	# This must come first in drawing. Put all other drawing commands below.
 	screen.fill((0,0,0))
 
-	# Draw bricks, blue with white border
-	for brick in bricks:
-		pygame.draw.rect(screen, (0,64,255), brick)
-		pygame.draw.rect(screen, (200,200,255), brick, 2)
+	# Draw blocks, blue with white border
+	for block in blocks:
+		pygame.draw.rect(screen, pygame.Color(block_color), block)
+		pygame.draw.rect(screen, (200,200,255), block, 2)
 
 	if game_on:
-		# Move and draw the paddle
-		paddle.left += paddle_dx
-		pygame.draw.rect(screen, (255, 255, 255), paddle)
+		# Draw the paddle
+		pygame.draw.rect(screen, pygame.Color(paddle_color), paddle)
 
-		# Move and draw the ball
-		ball.centerx += ball_dx
-		ball.centery += ball_dy
-		pygame.draw.circle(screen, (255,64,0), ball.center, ball.width/2)
+		# Draw the ball
+		pygame.draw.circle(screen, pygame.Color(ball_color), ball.center, ball.width/2)
 
-	# Show "Game Over" in the center if the player lost.
+	# Show ending message
 	else:
-		font = pygame.font.Font(None, 36)
-		text = font.render(end_message, 1, (255,255,255))
-		textpos = text.get_rect()
-		textpos.center = game_board.center
-		screen.blit(text, textpos)
+		draw_text(end_message, game_board.center)
 
-	# Show the frame
+	# Show the screen. 
+	# This must come last, put all other drawing commands above.
 	pygame.display.flip()
 
